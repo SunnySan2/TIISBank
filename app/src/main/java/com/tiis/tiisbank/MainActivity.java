ba2728ba38e22260e0a7795845d48ced1e60b576
SunnySan2 <56286833+SunnySan2@users.noreply.github.com>
2019-10-09 16:34:40 +0800

    office

--- app/src/main/java/com/tiis/tiisbank/MainActivity.java
+++ app/src/main/java/com/tiis/tiisbank/MainActivity.java
@@ -2,9 +2,21 @@ package com.tiis.tiisbank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
 
 public class MainActivity extends AppCompatActivity {
     //CarouselView圖片輪播，參考 https://github.com/sayyam/carouselview
@@ -15,5 +27,151 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setMenuIcon();
+        setOnClickListener();
     }
+
+    // 加入載入選單資源的方法
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    // 使用者選擇所有的選單項目都會呼叫這個方法
+    public void clickMenuItem(MenuItem item) {
+        // 使用參數取得使用者選擇的選單項目元件編號
+        int itemId = item.getItemId();
+
+        // 判斷該執行什麼工作，目前還沒有加入需要執行的工作
+        switch (itemId) {
+            case R.id.about_tiis:
+                AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+                dialog.setTitle(R.string.msgSystemInfo)
+                        .setMessage(R.string.aboutTiis)
+                        .setIcon(R.drawable.info)
+                        .show();
+                break;
+        }
+
+    }
+
+    //設定輪播 banner
+    private void setCarouselView(){
+        carouselView = (CarouselView) findViewById(R.id.carouselView);
+        carouselView.setPageCount(bannerImages.length);
+        carouselView.setImageListener(imageListener);
+        ViewGroup.LayoutParams para = carouselView.getLayoutParams();
+        // 取得螢幕解析度
+        DisplayMetrics dm = new DisplayMetrics();
+        this.getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int i = dm.widthPixels;
+        //Log.d("FCM", "i=" + String.valueOf(i));
+        //i = (108*i/730)-32; //原始檔案是730*108的，減32是因為Layout有paddingLeft及paddingRight各16
+        //i = (i/2)-32; //原始檔案是730*108的，減32是因為Layout有paddingLeft及paddingRight各16
+        i = i/2;
+        //Log.d("FCM", "i=" + String.valueOf(i));
+        para.height = i;
+        carouselView.setLayoutParams(para);
+
+        i=dm.heightPixels;
+        Log.d("FCM", "i=" + String.valueOf(i));
+        i=(i-para.height-32)/2;
+        Log.d("FCM", "i=" + String.valueOf(i));
+        //para.height = i;
+        //Button btn = (Button) findViewById(R.id.iconRegistration);
+        //btn.setHeight(i);
+        //btn.setLayoutParams(para);
+    }   //private void setCarouselView(){
+
+    //給輪播 banner用的
+    ImageListener imageListener = new ImageListener() {
+        @Override
+        public void setImageForPosition(int position, ImageView imageView) {
+            imageView.setImageResource(bannerImages[position]);
+        }
+    };
+
+    private void setMenuIcon(){
+        ImageButton btn = null;
+        String sCountry = "";
+        int  sysVersion = Build.VERSION.SDK_INT;
+        if (sysVersion>23){
+            sCountry = getResources().getConfiguration().getLocales().get(0).getCountry();
+        }else{
+            sCountry = getResources().getConfiguration().locale.getCountry();
+        }
+        Log.d("FCM", "sCountry=" + sCountry);
+        if (sCountry.equals("TW")){    //繁體中文，台灣
+            bannerImages = new int[] {R.drawable.banner_tw_01, R.drawable.banner_tw_02, R.drawable.banner_tw_03};
+
+            btn = (ImageButton) findViewById(R.id.iconRegistration);
+            btn.setBackgroundResource(R.drawable.menu_registration_tw);
+            btn = (ImageButton) findViewById(R.id.iconMyCertificate);
+            btn.setBackgroundResource(R.drawable.menu_certificate_tw);
+            btn = (ImageButton) findViewById(R.id.iconReport);
+            btn.setBackgroundResource(R.drawable.menu_report_tw);
+            btn = (ImageButton) findViewById(R.id.iconChangePinCode);
+            btn.setBackgroundResource(R.drawable.menu_pin_tw);
+        }else{
+            bannerImages = new int[] {R.drawable.banner_tw_01, R.drawable.banner_tw_02, R.drawable.banner_tw_03};
+            btn = (ImageButton) findViewById(R.id.iconRegistration);
+            btn.setBackgroundResource(R.drawable.menu_registration_us);
+            btn = (ImageButton) findViewById(R.id.iconMyCertificate);
+            btn.setBackgroundResource(R.drawable.menu_certificate_us);
+            btn = (ImageButton) findViewById(R.id.iconReport);
+            btn.setBackgroundResource(R.drawable.menu_report_us);
+            btn = (ImageButton) findViewById(R.id.iconChangePinCode);
+            btn.setBackgroundResource(R.drawable.menu_pin_us);
+        }
+        setCarouselView();
+    }
+
+    private void setOnClickListener(){
+        /*
+        ImageButton b1 = (ImageButton) findViewById(R.id.iconRegistration);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent();
+                i.setClass(MainActivity.this, RegistrationActivity.class);
+                startActivity(i);
+            }
+        });
+
+        ImageButton b2 = (ImageButton) findViewById(R.id.iconMyCertificate);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent();
+                i.setClass(MainActivity.this, MyCertActivity.class);
+                startActivity(i);
+            }
+        });
+
+        ImageButton b3 = (ImageButton) findViewById(R.id.iconReport);
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent();
+                i.setClass(MainActivity.this, ReportActivity.class);
+                startActivity(i);
+            }
+        });
+
+        ImageButton b4 = (ImageButton) findViewById(R.id.iconChangePinCode);
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent();
+                i.setClass(MainActivity.this, ChangePinCodeActivity.class);
+                startActivity(i);
+            }
+        });
+         */
+    }
+
+
+
+
 }
---
+++ app/src/main/java/com/tiis/tiisbank/utility.java
@@ -0,0 +1,62 @@
+package com.tiis.tiisbank;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.widget.Toast;
+
+public class utility {
+
+    public static String getMySetting(Context context, String keyName){
+        // 建立SharedPreferences物件
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        String valueString = sharedPreferences.getString(keyName, "");
+        return valueString;
+    }
+
+    public static void setMySetting(Context context, String keyName, String value){
+        // 建立SharedPreferences物件
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(keyName, value);
+        editor.apply();
+    }
+
+    public static void showToast(Context context, String msg) {
+        if (msg==null || msg.length()==0) return;
+        Toast.makeText(context, msg, Toast.LENGTH_LONG).show();
+    }
+
+    public static void showMessage(Context context, String msg){
+        AlertDialog.Builder dialog = new AlertDialog.Builder(context);
+        dialog.setTitle(R.string.msgSystemInfo)
+                .setMessage(msg)
+                .setIcon(R.drawable.ic_launcher)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .show();
+    }
+
+    //取得 byte array 每個 byte 的 16 進位碼
+    public static String byte2Hex(byte[] b) {
+        String result = "";
+        for (int i=0 ; i<b.length ; i++)
+            result += Integer.toString( ( b[i] & 0xff ) + 0x100, 16).substring( 1 );
+        return result;
+    }
+
+    //將 16 進位碼的字串轉為 byte array
+    public static byte[] hex2Byte(String hexString) {
+        byte[] bytes = new byte[hexString.length() / 2];
+        for (int i=0 ; i<bytes.length ; i++)
+            bytes[i] = (byte) Integer.parseInt(hexString.substring(2 * i, 2 * i + 2), 16);
+        return bytes;
+    }
+
+}
